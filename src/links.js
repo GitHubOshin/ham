901f736940db9d655bc84f6b6aadb1b46cb75cb7
OSHIN <izkp44@gmail.com>
2023-02-16 04:19:14 +0700

    Create links.js

---
+++ src/links.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    name: 'Mbow',
+    to: '/'
+  },
+  {
+    name: 'Nu',
+    to: '/'
+  },
+  {
+    name: 'Guitar',
+    to: '/'
+  },
+  {
+    name: 'Tintin',
+    to: '/'
+  }
+]
+
+export default links

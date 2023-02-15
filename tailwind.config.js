5780f0e1c4090e9ebbea35bed30bd3a213456dab
OSHIN <izkp44@gmail.com>
2023-02-16 04:22:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      screen350: '350px',
+      screen750: '750px'
+    },
     extend: {}
   },
   plugins: []

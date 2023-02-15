42e5a6afec3f658601303fd263d7a7224288ca11
OSHIN <izkp44@gmail.com>
2023-02-16 04:21:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import Header from './components/Header'
+import { useState } from 'react'
+import Sidebar from './components/Sidebar'
+
 function App() {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
-    <div className="App">
-      <h1 className="text-3xl text-red-900 font-bold underline">Tailwind</h1>
+    <div className="w-screen h-screen bg-red-700">
+      {openMenu === true ? (
+        <Sidebar
+          closeMenu={() => {
+            setOpenMenu(false)
+          }}
+        />
+      ) : (
+        <Header
+          openMenu={() => {
+            setOpenMenu(true)
+          }}
+        />
+      )}
     </div>
   )
 }

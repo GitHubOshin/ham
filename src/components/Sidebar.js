9a36aa00bf07658a1d31749c771aaa7e022bf924
OSHIN <izkp44@gmail.com>
2023-02-16 09:12:08 +0700

    Set position to 'fixed'

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,15 +3,17 @@ import links from '../data/links'
 function Sidebar(props) {
   const { closeMenu } = props
   return (
-    <section className="relative bg-slate-800 w-[300px] h-screen py-20">
-      <button className="absolute top-4 right-4" onClick={closeMenu}>
-        <img alt="close" src="images/x-mark.svg" className="w-6 h-6" />
-      </button>
-      <nav className="w-full h-full text-white font-bold text-lg flex flex-col items-center gap-y-20">
-        {links.map((link) => {
-          return <a href={link.to}>{link.name}</a>
-        })}
-      </nav>
+    <section className="screen750:hidden fixed w-full h-full bg-[rgba(0,0,0,0.5)]">
+      <div className="relative bg-slate-200 w-[300px] h-full py-20">
+        <button className="absolute top-4 right-4" onClick={closeMenu}>
+          <img alt="close" src="images/x-mark.svg" className="w-6 h-6" />
+        </button>
+        <nav className="w-full h-full text-slate-700 font-bold text-lg flex flex-col items-center gap-y-20">
+          {links.map((link) => {
+            return <a href={link.to}>{link.name}</a>
+          })}
+        </nav>
+      </div>
     </section>
   )
 }

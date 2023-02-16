edb25c94d05991b680b7e7a21cb220ab78b8022e
OSHIN <izkp44@gmail.com>
2023-02-16 09:10:24 +0700

    Set position to 'fixed'

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import links from '../data/links'
 function Header(props) {
   const { openMenu } = props
   return (
-    <header className="min-w-[350px] w-full h-20 bg-slate-800 flex items-center p-10">
-      <button className="screen350:block screen750:hidden" onClick={openMenu}>
+    <header className="fixed w-full h-20 bg-slate-800 flex items-center p-10 box-border">
+      <button className="mobile:block screen750:hidden" onClick={openMenu}>
         <img alt="menu button" src="images/menu-icon.svg" />
       </button>
-      <nav className="screen350:hidden screen750:block text-white text-xl font-semibold w-full text-center ">
+      <nav className="mobile:hidden screen750:block text-white text-xl font-semibold w-full text-center ">
         {links.map((link, index) => {
           return (
             <a key={index} className="mx-10" href={link.to}>
